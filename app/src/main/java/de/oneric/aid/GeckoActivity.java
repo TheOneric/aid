b992b28d01ff71f59268f117c047089b81741ce3
Oneric <oneric@oneric.stub>
2020-08-30 17:52:29 +0200

    GeckoActivity: Actually load our webextension

--- app/src/main/java/de/oneric/aid/GeckoActivity.java
+++ app/src/main/java/de/oneric/aid/GeckoActivity.java
@@ -3,11 +3,14 @@ package de.oneric.aid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
+import org.mozilla.geckoview.GeckoResult;
 import org.mozilla.geckoview.GeckoRuntime;
 import org.mozilla.geckoview.GeckoSession;
 import org.mozilla.geckoview.GeckoView;
+import org.mozilla.geckoview.WebExtension;
 
 
 public class GeckoActivity extends AppCompatActivity {
@@ -38,6 +41,7 @@ public class GeckoActivity extends AppCompatActivity {
         GeckoSession session = new GeckoSession();
         if(geckoRuntime == null)
             geckoRuntime = GeckoRuntime.create(this);
+        setUpGecko(geckoRuntime, session);
 
         session.open(geckoRuntime);
         geckoView.setSession(session);
@@ -45,6 +49,25 @@ public class GeckoActivity extends AppCompatActivity {
 
         hideControl.onSystemUiVisibilityChange(View.SYSTEM_UI_FLAG_FULLSCREEN);
         getWindow().getDecorView().setOnSystemUiVisibilityChangeListener(hideControl);
+    }
+
+
+    private void setUpGecko(GeckoRuntime runtime, GeckoSession session) {
+        final String LOG_TAG = "setUpGecko";
+
+        geckoRuntime.getSettings().setRemoteDebuggingEnabled(true);
+        geckoRuntime.getSettings().setLoginAutofillEnabled(true);
+        GeckoResult<WebExtension> res = geckoRuntime.getWebExtensionController().ensureBuiltIn(
+                "resource://android/assets/aod-touchable/",
+                "aod-touchable@oneric.stub"
+        );
+        res.then(we -> {
+            Log.d(LOG_TAG, "Successfully Installed: "+we.id);
+            return GeckoResult.fromValue(0);
+        }, ex -> {
+            Log.e(LOG_TAG, "Webextension installation failed: " + ex.getMessage());
+            return GeckoResult.fromValue(0);
+        });
     }
 
 
